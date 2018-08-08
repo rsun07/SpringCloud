e48eb1ed3509fd1a6fa37983033eaca8185a3c44
Xiaoming Sun <rsun2014@gmail.com>
2018-08-08 15:34:26 -0700

    Add eureka client to web server, execlude jackson dependency from spring cloud to avoid conflict

--- springcloud-service/src/main/java/pers/xiaoming/springcloud/WebServiceApp.java
+++ springcloud-service/src/main/java/pers/xiaoming/springcloud/WebServiceApp.java
@@ -2,8 +2,12 @@ package pers.xiaoming.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class WebServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(WebServiceApp.class, args);
