d5af45220042e076c49c8b5ece372e873e0f895d
Xiaoming Sun <rsun2014@gmail.com>
2018-08-07 22:58:12 -0700

    update dependencies

--- springcloud-service/src/main/java/pers/xiaoming/springcloud/dao/IStudentDao.java
+++ springcloud-service/src/main/java/pers/xiaoming/springcloud/dao/IStudentDao.java
@@ -1,9 +1,11 @@
 package pers.xiaoming.springcloud.dao;
 
+import org.apache.ibatis.annotations.Mapper;
 import pers.xiaoming.springcloud.entity.Student;
 
 import java.util.List;
 
+@Mapper
 public interface IStudentDao {
     Student create(Student student);
 
