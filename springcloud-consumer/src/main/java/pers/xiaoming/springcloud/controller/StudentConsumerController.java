f2c030355dfc695027d73b7e94a1f911670b90a2
Xiaoming Sun <rsun2014@gmail.com>
2018-08-09 11:39:40 -0700

    enable hystrix to frontend consumer

--- springcloud-consumer/src/main/java/pers/xiaoming/springcloud/controller/StudentConsumerController.java
+++ springcloud-consumer/src/main/java/pers/xiaoming/springcloud/controller/StudentConsumerController.java
@@ -1,6 +1,9 @@
 package pers.xiaoming.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import pers.xiaoming.springcloud.FrontEndWebServiceApp;
 import pers.xiaoming.springcloud.entity.Student;
 
 import java.util.List;
@@ -24,8 +28,19 @@ public class StudentConsumerController {
 
     @RequestMapping
     @PostMapping
-    public Student post(@RequestBody Student student) {
-        return restTemplate.postForObject(REST_URL_HOST + STUDENT_RESOURCE, student, Student.class);
+    @HystrixCommand(fallbackMethod = "processHystrixPost")
+    public ResponseEntity<Student> post(@RequestBody Student student) {
+        if (student == null || student.getName() == null) {
+            throw new RuntimeException("Invalid student input");
+        }
+        restTemplate.postForObject(REST_URL_HOST + STUDENT_RESOURCE, student, Student.class);
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
+
+    // Fall back method must receive the same signature (return type, parameters) as the trigger method
+    // It shouldn't be a exception handler, here is just an example
+    public ResponseEntity<Student> processHystrixPost(@RequestBody Student student) {
+        return new ResponseEntity<>(student, HttpStatus.BAD_REQUEST);
     }
 
     @RequestMapping(value = "/id/{id}")
