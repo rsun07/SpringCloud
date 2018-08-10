eccb20185b24c3532f4a490ae5e78d51b9352ed3
Xiaoming Sun <rsun2014@gmail.com>
2018-08-09 17:42:20 -0700

    refactor consumer hystrix

--- springcloud-consumer-hystrix/src/main/java/pers/xiaoming/springcloud/hystrix/controller/StudentConsumerController.java
+++ springcloud-consumer-hystrix/src/main/java/pers/xiaoming/springcloud/hystrix/controller/StudentConsumerController.java
@@ -1,6 +1,7 @@
 package pers.xiaoming.springcloud.hystrix.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/student_consumer/hystrix")
+@Slf4j
 public class StudentConsumerController {
 
     private static final String REST_URL_HOST = "http://localhost:8001";
@@ -34,19 +36,21 @@ public class StudentConsumerController {
             throw new RuntimeException("Invalid student input");
         }
         restTemplate.postForObject(REST_URL_HOST + STUDENT_RESOURCE, student, Student.class);
-        return new ResponseEntity<>(student, HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).body(student);
     }
 
     // Fall back method must receive the same signature (return type, parameters) as the trigger method
-    // It shouldn't be a exception handler, here is just an example
+    // It shouldn't be used as a exception handler
     public ResponseEntity<Student> processHystrixPost(@RequestBody Student student) {
-        return new ResponseEntity<>(student, HttpStatus.BAD_REQUEST);
+        log.error("Fail fast with 500 and return anonymous user");
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(new Student("", 0.0));
     }
 
     @RequestMapping(value = "/id/{id}")
     @GetMapping
-    public Student get(@PathVariable("id") int id) {
-        return new GetStudentById(restTemplate, id).execute();
+    public ResponseEntity<Student> get(@PathVariable("id") int id) {
+        Student student = new GetStudentById(restTemplate, id).execute();
+        return ResponseEntity.ok(student);
     }
 
     @SuppressWarnings("unchecked")
